495ac73e6671d4a7a3fffb4629f99ac14071a990
TremerYT <jefferymutuku283@gmail.com>
2026-01-07 23:22:17 +0300

    changes Made: - Created the product controller.

--- src/main/java/com/ims/inventory_management_system_backend/controller/product/ProductController.java
+++ src/main/java/com/ims/inventory_management_system_backend/controller/product/ProductController.java
@@ -1,6 +1,13 @@
 package com.ims.inventory_management_system_backend.controller.product;
 
+import com.ims.inventory_management_system_backend.dto.product.ProductRequestDTO;
+import com.ims.inventory_management_system_backend.dto.product.ProductResponseDTO;
+import com.ims.inventory_management_system_backend.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
 
+    @PostMapping("/create")
+    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductRequestDTO productRequest) {
+        ProductResponseDTO createdProduct = productService.createProduct(productRequest);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
 }
