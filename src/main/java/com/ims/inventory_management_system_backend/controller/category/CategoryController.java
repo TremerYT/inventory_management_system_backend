2c69824523bea17e043fddc2ba86069232db95a5
TremerYT <jefferymutuku283@gmail.com>
2026-01-18 22:35:16 +0300

    changes Made: - Finished the Controller for category.

--- src/main/java/com/ims/inventory_management_system_backend/controller/category/CategoryController.java
+++ src/main/java/com/ims/inventory_management_system_backend/controller/category/CategoryController.java
@@ -1,11 +1,43 @@
 package com.ims.inventory_management_system_backend.controller.category;
 
+import com.ims.inventory_management_system_backend.dto.category.CategoryRequestDTO;
+import com.ims.inventory_management_system_backend.dto.category.CategoryResponseDTO;
+import com.ims.inventory_management_system_backend.service.category.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/category")
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponseDTO>> getAllCategories () {
+        return ResponseEntity.ok(categoryService.getCategories());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CategoryResponseDTO> createCategory(@Valid @RequestBody CategoryRequestDTO request){
+        CategoryResponseDTO category = categoryService.createCategory(request);
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponseDTO> updateCategory(
+            @Valid @RequestBody CategoryRequestDTO request,
+            @PathVariable Long id) {
+        return ResponseEntity.ok(categoryService.updateCategory(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id){
+        categoryService.deleteCategory(id);
+    }
 }
