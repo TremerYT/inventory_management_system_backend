4207a7a38297695931c9251e1cdccff05b4e383b
TremerYT <jefferymutuku283@gmail.com>
2026-02-08 13:13:43 +0300

    changes Made: - Created brand entity.

---
+++ src/main/java/com/ims/inventory_management_system_backend/entities/brands/Brand.java
@@ -0,0 +1,43 @@
+package com.ims.inventory_management_system_backend.entities.brands;
+
+import com.ims.inventory_management_system_backend.entities.product.Product;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "brand_code", nullable = false, unique = true)
+    private String brandCode;
+
+    @Column(name = "brand_name", unique = true, nullable = false)
+    private String brandName;
+
+    @Column(name = "brand_image", unique = true, nullable = false)
+    private String brandImage;
+
+    @Column(name = "is_active", nullable = false)
+    private Boolean isActive;
+
+    @OneToMany(mappedBy = "brands")
+    private List<Product> products;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
