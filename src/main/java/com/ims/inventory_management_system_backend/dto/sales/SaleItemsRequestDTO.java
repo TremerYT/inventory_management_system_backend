62cbe95b4c28bb0abbf6602be3099634da15c6cb
TremerYT <jefferymutuku283@gmail.com>
2026-02-18 12:47:56 +0300

    changes Made: - Create Sales DTO

---
+++ src/main/java/com/ims/inventory_management_system_backend/dto/sales/SaleItemsRequestDTO.java
@@ -0,0 +1,8 @@
+package com.ims.inventory_management_system_backend.dto.sales;
+
+public class SaleItemsRequestDTO {
+    private Long productId;
+    private Integer quantity;
+    private Double unitPrice;
+    private Double discount;
+}
---
+++ src/main/java/com/ims/inventory_management_system_backend/dto/sales/SaleRequestDTO.java
@@ -0,0 +1,37 @@
+package com.ims.inventory_management_system_backend.dto.sales;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class SaleRequestDTO {
+    @NotBlank(message = "Reference number is required")
+    public String referenceNumber;
+
+    @NotBlank(message = "Date is required")
+    public LocalDate date;
+
+    @NotNull(message = "Customer name is required")
+    public Long customerId;
+
+    @NotNull(message = "Shipping is required")
+    public Double shipping;
+
+    @NotNull(message = "Paid is required")
+    public Double paid;
+
+    @NotBlank(message = "Sale status is required")
+    public String saleStatus;
+
+    @NotBlank(message = "Payment status is required")
+    public String paymentStatus;
+
+    public String remarks;
+
+    @NotNull(message = "Items are required")
+    public List<SaleItemsRequestDTO> items;
+}
