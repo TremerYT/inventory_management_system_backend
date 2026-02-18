bfeaabf8c1544052fdfcf303d43fe3950743e9f3
TremerYT <jefferymutuku283@gmail.com>
2026-02-18 12:48:54 +0300

    changes Made: - Created Sale Items

---
+++ src/main/java/com/ims/inventory_management_system_backend/entities/sale/SaleItems.java
@@ -0,0 +1,45 @@
+package com.ims.inventory_management_system_backend.entities.sale;
+
+import com.ims.inventory_management_system_backend.entities.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sale_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SaleItems {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "sale_id", nullable = false)
+//    private Sales sale;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "product_id", nullable = false)
+//    private Product product;
+//
+//    @Column(name = "quantity", nullable = false)
+//    private Integer quantity;
+//
+//    @Column(name = "unit_price", nullable = false)
+//    private Double unitPrice;
+//
+//    @Column(name = "discount", nullable = false)
+//    private Double discount;
+//
+//    @Column(name = "sub_total", nullable = false)
+//    private Double subTotal;
+//
+//    @Column(name = "created_at", nullable = false, updatable = false)
+//    @CreationTimestamp
+//    private LocalDateTime createdAt;
+}
