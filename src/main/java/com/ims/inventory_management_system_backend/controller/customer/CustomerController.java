180597e7bc4002b9ef314c40571a0001af193f2d
TremerYT <jefferymutuku283@gmail.com>
2026-02-20 21:51:54 +0300

    changes Made: - Created customer controller

---
+++ src/main/java/com/ims/inventory_management_system_backend/controller/customer/CustomerController.java
@@ -0,0 +1,40 @@
+package com.ims.inventory_management_system_backend.controller.customer;
+
+import com.ims.inventory_management_system_backend.dto.customer.CustomerRequestDTO;
+import com.ims.inventory_management_system_backend.dto.customer.CustomerResponseDTO;
+import com.ims.inventory_management_system_backend.service.customer.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerResponseDTO> createCustomer(@Valid @RequestBody CustomerRequestDTO request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponseDTO>> getAllCustomers() {
+        return ResponseEntity.ok(customerService.getAllCustomers());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerResponseDTO> updateCustomer(@PathVariable Long id, @Valid @RequestBody CustomerRequestDTO request) {
+        return ResponseEntity.ok(customerService.updateCustomer(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return ResponseEntity.noContent().build();
+    }
+}
