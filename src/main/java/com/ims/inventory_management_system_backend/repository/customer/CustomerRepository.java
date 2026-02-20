5601f436034082a300cd30653dd80939bb2f558c
TremerYT <jefferymutuku283@gmail.com>
2026-02-20 21:52:24 +0300

    changes Made: - Created customer repository to include count

--- src/main/java/com/ims/inventory_management_system_backend/repository/customer/CustomerRepository.java
+++ src/main/java/com/ims/inventory_management_system_backend/repository/customer/CustomerRepository.java
@@ -2,6 +2,9 @@ package com.ims.inventory_management_system_backend.repository.customer;
 
 import com.ims.inventory_management_system_backend.entities.customers.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query("SELECT COUNT(c) FROM Customer c WHERE DATE(c.createdAt) == CURRENT_DATE ")
+    long countCustomerCreatedToday();
 }
