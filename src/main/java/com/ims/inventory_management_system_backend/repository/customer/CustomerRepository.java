fb4fcea1d47457f287bc2004c59f29a1201ea13c
TremerYT <jefferymutuku283@gmail.com>
2026-02-21 17:27:04 +0300

    changes Made: - Fixed equality issue

--- src/main/java/com/ims/inventory_management_system_backend/repository/customer/CustomerRepository.java
+++ src/main/java/com/ims/inventory_management_system_backend/repository/customer/CustomerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    @Query("SELECT COUNT(c) FROM Customer c WHERE DATE(c.createdAt) == CURRENT_DATE ")
+    @Query("SELECT COUNT(c) FROM Customer c WHERE CAST(c.createdAt AS date) = CURRENT_DATE")
     long countCustomerCreatedToday();
 }
